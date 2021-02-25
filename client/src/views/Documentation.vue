f4138fbec1e117f0b8b7924787c49264624baf8f
currymike <currymike@gmail.com>
2021-02-25 18:35:16 -0500

    Class updates 2/25

---
+++ client/src/views/Documentation.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class container>
+      <h1>
+          Party we are on the documentation page
+      </h1>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
