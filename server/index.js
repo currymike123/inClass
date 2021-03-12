4e814ba59fe111703286059264933e1d85ea7c22
currymike <currymike@gmail.com>
2021-03-12 09:52:58 -0500

    Class 3/12/21

--- server/index.js
+++ server/index.js
@@ -1,21 +1,20 @@
-const path = require('path');
-const express = require('express')
-const app = express()
-const port = 3000
+const path = require("path");
+const express = require("express");
+
+const userCtrl = require("./controllers/users");
+
+const app = express();
+const port = 3000;
 
 app
-    .use(express.static('./docs'))
-    .get('/', (req, res) => {
-    res.send('Hello World!')
-    })
-    .get(`/purim`, (req,res) => {
-        res.send("Hello");
-    })
-    //All the way at the end of the pipeline.  Return instead of not found
-    .get('*', (req, res) => {
-      res.sendFile( path.join(__dirname,'../docs/index.html'));
-    }
+  .use(express.static("./docs"))
+
+  .use("/users", usersCtrl)
+  //All the way at the end of the pipeline.  Return instead of not found
+  .get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../docs/index.html"));
+  });
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+  console.log(`Example app listening at http://localhost:${port}`);
+});
