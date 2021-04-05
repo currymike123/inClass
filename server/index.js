c1e93e7d7fa03b404ecc4570c062c5e66209bc48
currymike <currymike@gmail.com>
2021-04-05 10:16:25 -0400

    Practice Server

--- server/index.js
+++ server/index.js
@@ -1,20 +1,46 @@
-const path = require("path");
-const express = require("express");
+/*  B"H
+*/
 
-const userCtrl = require("./controllers/users");
+const path = require('path');
+const express = require('express');
+const dotenv = require('dotenv');
+dotenv.config();
+
+const { LoginRequired  } = require('./controllers/security');
+const usersModel = require('./models/users');
+const usersCtrl = require('./controllers/users');
+const postsCtrl = require('./controllers/posts');
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app
-  .use(express.static("./docs"))
+    .use(express.json())
+    .use(express.static('./docs'))
 
-  .use("/users", usersCtrl)
-  //All the way at the end of the pipeline.  Return instead of not found
-  .get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "../docs/index.html"));
-  });
+    .use((req, res, next)=>{ 
+        
+        const token = req.headers.authorization?.split(' ')[1];
+        req.user = token && usersModel.FromJWT(token);
+        next();
+    }) 
+
+    .use('/users', usersCtrl)
+    .use('/posts', LoginRequired, postsCtrl)
+
+
+    // All the way at the end of the pipeline. Return instead of not found
+    .get('*', (req, res) => {
+        res.sendFile( path.join(__dirname, '../docs/index.html' ) );
+    })
+
+    .use((error, req, res, next)=>{
+      console.error(error);
+
+        res.status(error.code || 500 );
+        res.send( { msg: error.msg });
+    })
 
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+  console.log(`Example app listening at http://localhost:${port}`)
+})
--- server/models/users.js
+++ server/models/users.js
@@ -1,3 +1,107 @@
-const list = [{ name: "Mike Curry" }];
+/* B"H
 
-module.exports.GetAll = () => list;
+*/
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+const SALT_ROUNDS = process.env.SALT_ROUNDS;
+const JWT_SECRET = process.env.JWT_SECRET;
+
+const list = [
+    { 
+        firstName: 'Moshe',
+        lastName: 'Plotkin',
+        handle: '@JewPaltz',
+        pic: 'https://bulma.io/images/placeholders/96x96.png',
+        password: 'Me',
+    },
+    { 
+        firstName: 'Kamala',
+        lastName: 'Harris',
+        handle: '@vp',
+        pic: 'https://bulma.io/images/placeholders/96x96.png',
+        password: 'Her',
+    },
+    { 
+        firstName: 'John',
+        lastName: 'Smith',
+        handle: '@johnsmith',
+        pic: 'https://bulma.io/images/placeholders/96x96.png',
+        password: 'BeepBop',
+    },
+
+];
+
+module.exports.GetAll = ()=> list;
+module.exports.Get = (user_id)=> list[user_id];
+module.exports.GetByHandle = (handle)=> ({ ...list.find( (x, i)=> x.handle == handle ), password: undefined }) ;
+module.exports.Add = (user)=> {
+    if(!user.firstName){
+        throw { code: 422, msg: "First Name is required" }
+    }
+     list.push(user);
+     return { ...user, password: undefined };
+}
+module.exports.Register = async (user)=> {
+
+    const hash = await bcrypt.hash(user.password, +SALT_ROUNDS);
+
+    user.password = hash;
+
+    if(!user.firstName){
+        throw { code: 422, msg: "First Name is required" }
+    }
+
+    list.push(user);
+    return { ...user, password: undefined };
+
+}
+module.exports.Update = (user_id, user)=> {
+    const oldObj = list[user_id];
+    if(user.firstName){
+        oldObj.firstName = user.firstName;
+    }
+    if(user.lastName){
+        oldObj.lastName = user.lastName;
+    }
+    if(user.handle){
+        oldObj.handle = user.handle;
+    }
+    if(user.pic){
+        oldObj.pic = user.pic;
+    }
+    //list[user_id] = newObj ;
+    return { ...oldObj, password: undefined };
+}
+module.exports.Delete = (user_id)=> {
+    const user = list[user_id];
+    list.splice(user_id, 1);
+    return user;
+}
+
+module.exports.Login = async (handle, password) =>{
+    console.log({ handle, password})
+    const user = list.find(x=> x.handle == handle);
+    if(!user) throw { code: 401, msg: "Sorry there is no user with that handle" };
+
+    if( ! await bcrypt.compare(password, user.password) ){
+        throw { code: 401, msg: "Wrong Password" };
+    }
+
+    const data = { ...user, password: undefined };
+
+    const token = jwt.sign(data, JWT_SECRET)
+
+    return { user, token };
+}
+
+module.exports.FromJWT = async (token) =>{
+    try {
+        const user = jwt.verify(token, JWT_SECRET);
+        return user;       
+    } catch (error) {
+        console.log({error});
+        return null;
+    }
+
+}
